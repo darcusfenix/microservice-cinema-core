1ae6ae497fd15d5f381587cb17d2d01fa51f1cf0
Juan Crisóstomo <juancvfenix@gmail.com>
2017-03-22 22:27:49 -0600

    cambio en config mongodb

--- dist/configuration/index.js
+++ dist/configuration/index.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 var serverSettings = exports.serverSettings = {
     "nameServer": "localhost",
     "mongodbUrl": process.env.MONGODB_URL ? process.env.MONGODB_URL : "localhost",
-    "mongodbDatabase": process.env.MONGODB_DATABASE ? process.env.MONGODB_DATABASE : "admin",
-    "mongodbUser": process.env.MONGODB_USER ? process.env.MONGODB_USER : "darcusfenix",
-    "mongodbPassword": process.env.MONGODB_PASSWORD ? process.env.MONGODB_PASSWORD : "darcusfenix",
+    "mongodbDatabase": process.env.MONGODB_DATABASE ? process.env.MONGODB_DATABASE : "cinema",
+    "mongodbUser": process.env.MONGODB_USER ? process.env.MONGODB_USER : "",
+    "mongodbPassword": process.env.MONGODB_PASSWORD ? process.env.MONGODB_PASSWORD : "",
     "mongodbPort": process.env.MONGODB_PORT ? process.env.MONGODB_PORT : 27017,
     "expressPort": process.env.EXPRESS_PORT ? process.env.EXPRESS_PORT : 3000,
     "cache": {
