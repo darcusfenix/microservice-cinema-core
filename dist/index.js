d8649163df38f67a68f1d1bdda5987feeaddeada
Juan Crisóstomo <juancvfenix@gmail.com>
2017-03-22 22:19:11 -0600

    correcciones

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,33 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Configuration = exports.ConnectionProvider = exports.getCommentModel = exports.getMovieModel = exports.CommentSchema = exports.MovieSchema = undefined;
 
+var _MovieFactory = require("factories/MovieFactory");
+
+Object.defineProperty(exports, "getMovieModel", {
+  enumerable: true,
+  get: function get() {
+    return _MovieFactory.getMovieModel;
+  }
+});
+
+var _CommentFactory = require("factories/CommentFactory");
+
+Object.defineProperty(exports, "getCommentModel", {
+  enumerable: true,
+  get: function get() {
+    return _CommentFactory.getCommentModel;
+  }
+});
+
+var _index = require("configuration/index");
+
+Object.defineProperty(exports, "Configuration", {
+  enumerable: true,
+  get: function get() {
+    return _index.Configuration;
+  }
+});
+
 var _MovieModel = require("models/MovieModel");
 
 var _MovieModel2 = _interopRequireDefault(_MovieModel);
@@ -13,27 +40,12 @@ var _CommentModel = require("models/CommentModel");
 
 var _CommentModel2 = _interopRequireDefault(_CommentModel);
 
-var _MovieFactory = require("factories/MovieFactory");
-
-var _MovieFactory2 = _interopRequireDefault(_MovieFactory);
-
-var _CommentFactory = require("factories/CommentFactory");
-
-var _CommentFactory2 = _interopRequireDefault(_CommentFactory);
-
 var _connectionProvider = require("dataAccess/connectionProvider");
 
 var _connectionProvider2 = _interopRequireDefault(_connectionProvider);
-
-var _index = require("configuration/index");
-
-var _index2 = _interopRequireDefault(_index);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.MovieSchema = _MovieModel2.default;
 exports.CommentSchema = _CommentModel2.default;
-exports.getMovieModel = _MovieFactory2.default;
-exports.getCommentModel = _CommentFactory2.default;
 exports.ConnectionProvider = _connectionProvider2.default;
-exports.Configuration = _index2.default;
