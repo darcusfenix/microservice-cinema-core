a09ec9f0396fc1dfa09f6b77bd92f759e76a4a39
Juan Crisóstomo <juancvfenix@gmail.com>
2017-03-22 21:52:46 -0600

    se agregó el módulo connection provider

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Configuration = exports.Comment = exports.Movie = undefined;
+exports.ConectionProvider = exports.Configuration = exports.Comment = exports.Movie = undefined;
 
 var _MovieFactory = require("factories/MovieFactory");
 
@@ -13,8 +13,13 @@ var _index = require("configuration/index");
 
 var _index2 = _interopRequireDefault(_index);
 
+var _connectionProvider = require("dataAccess/connectionProvider");
+
+var _connectionProvider2 = _interopRequireDefault(_connectionProvider);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.Movie = _MovieFactory2.default;
 exports.Comment = _MovieFactory2.default;
 exports.Configuration = _index2.default;
+exports.ConectionProvider = _connectionProvider2.default;
--- src/index.es6
+++ src/index.es6
@@ -1,3 +1,4 @@
 export Movie from "factories/MovieFactory";
 export Comment from "factories/MovieFactory";
 export Configuration from "configuration/index";
+export ConnectionProvider from "dataAccess/connectionProvider";
